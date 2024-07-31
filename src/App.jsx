d68de30033ff38eda6743c2d52eb9855af170f74
Sheheryar <ssheheryar12@gmail.com>
2024-07-31 18:30:24 +0500

    fixedLoading

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,43 @@
 import React, { useState, useEffect } from "react";
 import { fetchUsers, getAvatarUrl } from "./services/Api";
 import UserList from "./components/UserList";
-import LoadingSpinner from "./components/LoadingSpinner";
+import StreachingSpinner from "./components/StreachingSpinner";
 import "./App.css";
 
 function App() {
   const [users, setUsers] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [delayTime, setDelayTime] = useState(false);
+
   useEffect(() => {
     const loadUsers = async () => {
-      const userData = await fetchUsers();
-      const usersAvatars = userData.map((user) => ({
-        ...user,
-        avatar: getAvatarUrl(user.username),
-      }));
-      setUsers(usersAvatars);
-      setLoading(false);
+      try {
+        const userData = await fetchUsers();
+        const usersAvatars = userData.map((user) => ({
+          ...user,
+          avatar: getAvatarUrl(user.username),
+        }));
+        setUsers(usersAvatars);
+        setLoading(false);
+      } catch (error) {
+        console.log(error);
+      }
     };
     loadUsers();
   }, []);
+  useEffect(() => {
+    const delay = 2000;
+    const timer = () => setDelayTime(true);
+    setTimeout(timer, delay);
+  }, []);
+
   return (
     <>
-      <div>{loading ? <LoadingSpinner /> : <UserList users={users} />}</div>
+      {loading || !delayTime ? (
+        <StreachingSpinner />
+      ) : (
+        <UserList users={users} />
+      )}
     </>
   );
 }
